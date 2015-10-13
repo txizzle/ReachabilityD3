97ca5c2a84882a51e98bb68934e6678a2d94abe2
Ted Xiao <t.xiao@berkeley.edu>
2015-10-12 21:28:11 -0700

    Working with lag

--- Iteration6/js/contours.js
+++ Iteration6/js/contours.js
@@ -4,60 +4,90 @@ var currLevel = 0;
 var contourDict = {};
 
 //initial svg contour
-d3.text("csv/t2z1.csv", function(text) {
-    var data = d3.csv.parseRows(text).map(function(row) {
-        return row.map(function(value) {
-            return -value;
-        });
-    });
-    //console.log(data);
-    var cliff = -1000;
-    data.push(d3.range(data[0].length).map(function() {
-        return cliff;
-    }));
-    data.unshift(d3.range(data[0].length).map(function() {
-        return cliff;
-    }));
-    data.forEach(function(d) {
-        d.push(cliff);
-        d.unshift(cliff);
-    });
+var margin = {top: 20, right: 80, bottom: 80, left: 80},
+width = 500 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
 
-    var c = new Conrec,
-        xs = d3.range(0, data.length),
-        ys = d3.range(0, data[0].length),
-        zs = [currLevel],
-        width = 300,
-        height = 300,
-        x = d3.scale.linear().range([0, width]).domain([0, data.length]),
-        y = d3.scale.linear().range([height, 0]).domain([0, data[0].length]),
-        colours = d3.scale.linear().domain([-5, 3]).range(["#fff",
-            "red"
-        ]);
+var xScale = d3.scale.linear()
+    .domain([0, 30])
+    .range([0, width]);
 
-    c.contour(data, 0, xs.length - 1, 0, ys.length - 1, xs, ys, zs.length,
-        zs);
-    var test = d3.select("#contour").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .selectAll("path")
-        .data(c.contourList())
-        .enter().append("path")
-        .style("fill", function(d) {
-            return colours(d.level);
-        })
-        .style("stroke", "black")
-        .attr("d", d3.svg.line()
-            .x(function(d) {
-                return x(d.x);
-            })
-            .y(function(d) {
-                return y(d.y);
-            }));
+var yScale = d3.scale.linear()
+    .domain([0, 30])
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+var line = d3.svg.line()
+    .interpolate("linear")
+    .x(function(d) { return xScale(d.x1); })
+    .y(function(d) { return yScale(d.y1); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+.attr("class", "axis")
+.attr("transform", "translate(0, " + height + ")")
+.call(xAxis);
+
+svg.append("g")
+.attr("class", "axis")
+.call(yAxis);
+
+d3.csv("csv/t1z1Data1.csv", function(mydata)
+{
+    svg.append("path")
+    .datum(mydata)
+    .attr("class", "line")
+    .attr("d", line);
+
 });
 
+d3.csv("csv/t1z1Data2.csv", function(mydata)
+{
+    svg.append("path")
+    .datum(mydata)
+    .attr("class", "line")
+    .attr("d", line);
+
+});
+    //
+    
+//    var data = d3.csv.parseRows(text).map(function(row) {
+//        return row.map(function(value) {
+//            return -value;
+//        });
+//    });
+//    var test = d3.select("#contour").append("svg")
+//        .attr("width", width)
+//        .attr("height", height)
+//        .selectAll("path")
+//        .data(c.contourList())
+//        .enter().append("path")
+//        .style("fill", function(d) {
+//            return colours(d.level);
+//        })
+//        .style("stroke", "black")
+//        .attr("d", d3.svg.line()
+//            .x(function(d) {
+//                return x(d.x);
+//            })
+//            .y(function(d) {
+//                return y(d.y);
+//            }));
+
 function updateContour(i) {
-    var key = currTime.toString() + "," + currEvader.toString();
+    var key = currTime.toString() + "," + currEvader.toString() + "Data";
     //if (key in contourDict) {
     if (1 == 2) { //temporariliy disable memoization
         var c = contourDict[key];
@@ -81,61 +111,63 @@ function updateContour(i) {
                 }));
     } else {
         currLevel = i;
-        var currFile = "csv/t" + currTime + "z" + currEvader + ".csv";
-        d3.text(currFile, function(text) {
-            var data = d3.csv.parseRows(text).map(function(row) {
-                return row.map(function(value) {
-                    return -value;
-                });
-            });
-            // console.log(data);
-            var cliff = -1000;
-            data.push(d3.range(data[0].length).map(function() {
-                return cliff;
-            }));
-            data.unshift(d3.range(data[0].length).map(function() {
-                return cliff;
-            }));
-            data.forEach(function(d) {
-                d.push(cliff);
-                d.unshift(cliff);
-            });
+        var currFile = "csv/t" + currTime + "z" + currEvader +"Data";
+        key = "t" + currTime + "z" + currEvader;
+        d3.select("svg").remove();
+        
+        var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var c = new Conrec,
-                xs = d3.range(0, data.length),
-                ys = d3.range(0, data[0].length),
-                zs = [i],
-                width = 300,
-                height = 300,
-                x = d3.scale.linear().range([0, width]).domain([0,
-                    data.length
-                ]),
-                y = d3.scale.linear().range([height, 0]).domain([0,
-                    data[0].length
-                ]);
+        svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxis);
+
+        svg.append("g")
+        .attr("class", "axis")
+        .call(yAxis);
+        
+        $.ajax({
+          url: currFile+"1.csv", //or your url
+          success: function(data){
+            console.log(">1 islands");
+            d3.csv(currFile+"1.csv", function(mydata)
+                {
+                    svg.append("path")
+                    .datum(mydata)
+                    .attr("class", "line")
+                    .attr("d", line);
+
+                });
+
+            d3.csv(currFile+"2.csv", function(mydata)
+            {
+                svg.append("path")
+                .datum(mydata)
+                .attr("class", "line")
+                .attr("d", line);
+
+            });
+          },
+          error: function(data){
+            debugger;
+            console.log("only one island");
+            d3.csv(currFile+".csv", function(mydata)
+            {
+                svg.append("path")
+                .datum(mydata)
+                .attr("class", "line")
+                .attr("d", line);
+
+            });
             
-            c.contour(data, 0, xs.length - 1, 0, ys.length - 1, xs,
-                ys, zs.length, zs);
-            
-            contourDict[key] = c;
-            d3.select("svg").remove();
-            var test = d3.select("#contour").append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .selectAll("path")
-                .data(c.contourList())
-                .enter().append("path")
-                .style("stroke", "black")
-                .attr("d", d3.svg.line()
-                    .x(function(d) {
-                        return x(d.x);
-                    })
-                    .y(function(d) {
-                        return y(d.y);
-                    }));
+          },
         });
     };
-}
+};
 
 var i = 0;
 
