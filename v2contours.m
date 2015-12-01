9d980ba9f0c301e7337f28775c17f3624f4568c0
Ted Xiao <t.xiao@berkeley.edu>
2015-12-01 14:54:41 -0800

    Changing v2contours to not plot every contour

--- v2contours.m
+++ v2contours.m
@@ -3,7 +3,7 @@ function v2contours(data)
     dimensions = size(size(data));
     is4d = (dimensions(2) == 5);
     
-    fileID = fopen('data_contours/twoislands.txt','w');
+    fileID = fopen('data_contours/twoislands.txt','a');
     len_x = length(data(:,1,1,1,1));
     len_y = length(data(1,:,1,1,1));
     len_3 = length(data(1,1,:,1,1));
@@ -19,47 +19,54 @@ function v2contours(data)
                 for var3=1:len_5
                     xx = 1:1:len_x;
                     yy = 1:1:len_y;
-                    [C,~] = contourf(xx,yy,data(:,:,var1,var2,var3), [0 0]);
+                    C = contourc(xx,yy,data(:,:,var1,var2,var3), [0 0]);
+
                     Csplit={};
                     breakpts = find(C(1,:)==0);
                     breakpts = [breakpts, size(C,2)+1];
                     for j=1:length(breakpts)-1
                         Csplit{j} = C(:,breakpts(j)+1:breakpts(j+1)-1);
                     end
-                    figure, hold on;
-                    for j=1:length(Csplit)
-                        plot(Csplit{j}(1,:),Csplit{j}(2,:));
-                    end
                     fn = strcat('t',int2str(var3), 'd', int2str(var2), 'z', int2str(var1));
-                    full_fn = strcat('data_contours/', fn);
-                    if (length(breakpts)-1 > 1)
+                    headers = {'x1', 'y1'};
+                    if length(breakpts) <= 2
+                        csvname = strcat('data_contours/', fn, 'Data.csv');
+                        csvwrite_with_headers(csvname',Csplit{1},headers);
+                    else
+                        for j=1:length(Csplit)
+                            csvname = strcat('data_contours/', fn, 'Data', int2str(j), '.csv');
+                            csvwrite_with_headers(csvname',Csplit{j},headers);
+                        end
+                    end
+                    
+                    if (length(breakpts) > 2)
                         fprintf(fileID,strcat(fn, '\n'));
                     end
-                    d3ify(gcf, full_fn);
-                    close all;
                 end
             else
                 xx = 1:1:len_x;
                 yy = 1:1:len_y;
                 
-                [C,~] = contourf(xx,yy,data(:,:,var1,var2), [0 0]);
+                C = contourc(xx,yy,data(:,:,var1,var2), [0 0]);
                 Csplit={};
                 breakpts = find(C(1,:)==0);
                 breakpts = [breakpts, size(C,2)+1];
                 for j=1:length(breakpts)-1
                     Csplit{j} = C(:,breakpts(j)+1:breakpts(j+1)-1);
                 end
-                figure, hold on;
-                for j=1:length(Csplit)
-                    plot(Csplit{j}(1,:),Csplit{j}(2,:));
-                end
                 fn = strcat('t',int2str(var2), 'z', int2str(var1));
-                full_fn = strcat('data_contours/', fn);
-                if (length(breakpts)-1 > 1)
+                if length(breakpts) <= 2
+                    csvname = strcat('data_contours/', fn, 'Data.csv');
+                    csvwrite_with_headers(csvname,Csplit{1},headers);
+                else
+                    for j=1:length(Csplit)
+                        csvname = strcat('data_contours/', fn, 'Data', int2str(j), '.csv');
+                        csvwrite_with_headers(csvname',Csplit{j},headers);
+                    end
+                end
+                if (length(breakpts) > 2)
                     fprintf(fileID,strcat(fn, '\n'));
                 end
-                d3ify(gcf, full_fn);
-                close all;
             end
         end
     end
